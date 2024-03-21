9a9a49e9df4ceb58847905310f73bf22347af2b4
holykremowka2137 <victoria2137@tutanota.com>
2024-03-21 15:17:51 +0100

    transfering my config to repo

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keyset = vim.keymap.set
+
+keyset({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- keyset({ 'n' }, '<C-\>>' )
+
+-- keyset('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- keyset('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+keyset("n", "[d", vim.diagnostic.goto_prev)
+keyset("n", "]d", vim.diagnostic.goto_next)
+keyset("n", "<leader>d", vim.diagnostic.open_float)
+keyset("n", "<leader>q", vim.diagnostic.setloclist)
+
+keyset("n", "<C-d>", "<C-d>zz")
+keyset("n", "<C-u>", "<C-u>zz")
+
+keyset("n", "n", "nzzzv")
+keyset("n", "N", "Nzzzv")
+
+keyset("n", "G", "Gzz")
+
+keyset("n", "<leader>ee", vim.cmd.Ex)
+keyset("n", "<leader>es", vim.cmd.Sex)
+keyset("n", "<leader>ev", vim.cmd.Vex)
+keyset("n", "<leader>el", vim.cmd.Lex)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+local g = vim.g
+
+opt.shell = "fish"
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.wrap = true
+opt.linebreak = true
+opt.number = true
+opt.relativenumber = false
+opt.scrolloff = 8
+opt.gdefault = true
+g.netrw_keepdir = 0
+g.netrw_banner = 0
+g.netrw_liststyle = 3
